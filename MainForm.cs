af14b469a8b6e468d8d8f6925dc8c4f8ad6ddc1d
Abdozakaria222 <abdozakaria222@gmail.com>
2025-12-01 03:53:25 +0200

    Update MainForm.cs

--- MainForm.cs
+++ MainForm.cs
@@ -4,7 +4,7 @@ using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Net.Http;
-using System.Text.Json;
+using Newtonsoft.Json;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
