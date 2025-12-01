eb68b930175e70375113eeb4f7685902def31db9
Abdozakaria222 <abdozakaria222@gmail.com>
2025-12-01 04:30:31 +0200

    Update MainForm.Designer.cs

--- MainForm.Designer.cs
+++ MainForm.Designer.cs
@@ -2,12 +2,15 @@ namespace newhhhh
 {
     partial class MainForm
     {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
         private System.ComponentModel.IContainer components = null;
-        private System.Windows.Forms.ListBox listBoxResults;
-        private System.Windows.Forms.Button btnTestDns;
-        private System.Windows.Forms.Button btnUpdateList;
-        private System.Windows.Forms.Button btnSaveResults;
 
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null))
@@ -17,6 +20,12 @@ namespace newhhhh
             base.Dispose(disposing);
         }
 
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
         private void InitializeComponent()
         {
             this.listBoxResults = new System.Windows.Forms.ListBox();
@@ -24,16 +33,18 @@ namespace newhhhh
             this.btnUpdateList = new System.Windows.Forms.Button();
             this.btnSaveResults = new System.Windows.Forms.Button();
             this.SuspendLayout();
-            
+            // 
             // listBoxResults
+            // 
             this.listBoxResults.FormattingEnabled = true;
             this.listBoxResults.ItemHeight = 16;
             this.listBoxResults.Location = new System.Drawing.Point(12, 12);
             this.listBoxResults.Name = "listBoxResults";
             this.listBoxResults.Size = new System.Drawing.Size(560, 340);
             this.listBoxResults.TabIndex = 0;
-            
+            // 
             // btnTestDns
+            // 
             this.btnTestDns.Location = new System.Drawing.Point(12, 370);
             this.btnTestDns.Name = "btnTestDns";
             this.btnTestDns.Size = new System.Drawing.Size(120, 40);
@@ -41,8 +52,9 @@ namespace newhhhh
             this.btnTestDns.Text = "اختبار DNS";
             this.btnTestDns.UseVisualStyleBackColor = true;
             this.btnTestDns.Click += new System.EventHandler(this.btnTestDns_Click);
-            
+            // 
             // btnUpdateList
+            // 
             this.btnUpdateList.Location = new System.Drawing.Point(150, 370);
             this.btnUpdateList.Name = "btnUpdateList";
             this.btnUpdateList.Size = new System.Drawing.Size(120, 40);
@@ -50,8 +62,9 @@ namespace newhhhh
             this.btnUpdateList.Text = "تحديث القائمة";
             this.btnUpdateList.UseVisualStyleBackColor = true;
             this.btnUpdateList.Click += new System.EventHandler(this.btnUpdateList_Click);
-            
+            // 
             // btnSaveResults
+            // 
             this.btnSaveResults.Location = new System.Drawing.Point(288, 370);
             this.btnSaveResults.Name = "btnSaveResults";
             this.btnSaveResults.Size = new System.Drawing.Size(120, 40);
@@ -59,8 +72,9 @@ namespace newhhhh
             this.btnSaveResults.Text = "حفظ النتائج";
             this.btnSaveResults.UseVisualStyleBackColor = true;
             this.btnSaveResults.Click += new System.EventHandler(this.btnSaveResults_Click);
-            
+            // 
             // MainForm
+            // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(584, 430);
@@ -71,6 +85,14 @@ namespace newhhhh
             this.Name = "MainForm";
             this.Text = "DNS Tester";
             this.ResumeLayout(false);
+
         }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBoxResults;
+        private System.Windows.Forms.Button btnTestDns;
+        private System.Windows.Forms.Button btnUpdateList;
+        private System.Windows.Forms.Button btnSaveResults;
     }
 }
